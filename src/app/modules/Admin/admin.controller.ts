074af4fd14a92e5329faec8a9a59f00799446200
aminul <rahathssn@gmail.com>
2025-01-26 16:45:17 +0600

    added Applying Pagination to Results

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,11 +1,16 @@
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
 
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
-    const filters = pick(req.query, ["name", "email", "searchTerm"]);
-    const result = await AdminService.getAllFromDB(filters);
+    const filters = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ["limit", "page"]);
+
+    console.log(options);
+
+    const result = await AdminService.getAllFromDB(filters, options);
 
     res.status(200).json({
       success: true,
