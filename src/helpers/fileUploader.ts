29305041ee33c167ff677804463e2343e2708cea
aminul <rahathssn@gmail.com>
2025-02-04 14:31:22 +0600

    added Implementing Functionality for Image Upload to Cloudinary

--- src/helpers/fileUploader.ts
+++ src/helpers/fileUploader.ts
@@ -1,5 +1,12 @@
 import multer from "multer";
 import path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: "dhpc4eqmf",
+  api_key: "375585789496921",
+  api_secret: "BtDSd_2NBqOr9yfzH7d1VMspDAE",
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -12,6 +19,26 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+// Upload an image
+const uploadToCloudinary = async (file: any) => {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      file.path,
+      {
+        public_id: file.originalname,
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
+
 export const fileUploader = {
   upload,
+  uploadToCloudinary,
 };
