11e4c1457695b94a6a4b012e96d8d3775c2e195e
aminul <rahathssn@gmail.com>
2025-02-05 13:25:17 +0600

    added new branch to resolve the secret issue

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -6,34 +6,34 @@ import { fileUploader } from "../../../helpers/fileUploader";
 const createAdmin = async (req: any) => {
   const file = req.file;
 
+  console.log(req.body);
+
   if (file) {
     const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
-    req.body.data.admin.profilePhoto = uploadToCloudinary?.secure_url;
-
-    console.log(req.body.data);
+    req.body.admin.profilePhoto = uploadToCloudinary?.secure_url;
   }
 
-  // const hashedPassword: string = await bcrypt.hash(data.password, 12);
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 12);
 
-  // const userData = {
-  //   email: data.admin.email,
-  //   password: hashedPassword,
-  //   role: UserRole.ADMIN,
-  // };
+  const userData = {
+    email: req.body.admin.email,
+    password: hashedPassword,
+    role: UserRole.ADMIN,
+  };
 
-  // const result = await prisma.$transaction(async (transactionClient) => {
-  //   await transactionClient.user.create({
-  //     data: userData,
-  //   });
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
 
-  //   const createdAdminData = await transactionClient.admin.create({
-  //     data: data.admin,
-  //   });
+    const createdAdminData = await transactionClient.admin.create({
+      data: req.body.admin,
+    });
 
-  //   return createdAdminData;
-  // });
+    return createdAdminData;
+  });
 
-  // return result;
+  return result;
 };
 
 export const userService = {
