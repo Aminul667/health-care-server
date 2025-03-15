ed5b6936bfafc2bfceb227d2a823119d06a167d6
aminul <rahathssn@gmail.com>
2025-03-15 15:00:14 +0600

    added appointment cancel service

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import router from "./app/routes";
 import httpStatus from "http-status";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import cookieParser from "cookie-parser";
+import cron from "node-cron";
+import { AppointmentService } from "./app/modules/Appointment/appointment.service";
 
 const app: Application = express();
 app.use(cors());
@@ -12,6 +14,14 @@ app.use(cookieParser());
 // parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+cron.schedule("* * * * *", () => {
+  try {
+    AppointmentService.cancelUnpaidAppointments();
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
